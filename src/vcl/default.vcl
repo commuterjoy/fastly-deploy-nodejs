dd8d88e3d0a6d1fcb5c987332908d91a2f3559df
Matt Chadburn <matt.chadburn@ft.com>
2014-08-06 15:18:49 +0100

    Start auth, caching and metrics

--- src/vcl/default.vcl
+++ src/vcl/default.vcl
@@ -1,27 +1,54 @@
+
 backend next {
     .first_byte_timeout = 15s;
     .connect_timeout = 3s;
     .share_key = "f8585BOxnGQDMbnkJoM1e";
     .port = "80";
-    .host = "62.25.64.110"; // ft.com 
-}
-
-backend webapp {
-    .first_byte_timeout = 15s;
-    .connect_timeout = 3s;
-    .share_key = "f8585BOxnGQDMbnkJoM1e";
-    .port = "80";
     .host = "54.217.215.190"; // commuterjoy.co.uk
+    .probe = {
+            .url = "/";
+            .interval = 20s;
+            .timeout = 2s;
+            .window = 5;
+            .threshold = 3;
+    }
 }
 
 sub vcl_recv {
   
-  # default backend
-  set req.backend = next;
+    # Default backend
+    set req.backend = next;
 
-  # user-agent sent to the webapp
-  if (req.http.User-Agent ~ "(?i)iphone") {
-    set req.backend = webapp;
-  }
-    
+    if (req.restarts == 0) {
+        if (!req.http.X-Timer) {
+            set req.http.X-Timer = "S" time.start.sec "." time.start.usec_frac;
+        }
+        set req.http.X-Timer = req.http.X-Timer ",VS0";
+    }
+
+    # Block access to anyone without the secret key
+    if (req.http.X-FT-Secret != "HoratioBottomley") {
+        error 403 "Forbidden";
+    }
+
+    # Don't cache uncachable requests
+    if (req.request != "HEAD" && req.request != "GET" && req.request != "FASTLYPURGE") {
+        set req.http.X-FT-Cachable = "false";
+        return(pass);
+    }
+
+    return (lookup);
+
+}
+
+sub vcl_deliver {
+
+    if (req.http.X-FT-Cachable) {
+        set resp.http.X-FT-Cachable = req.http.X-FT-Cachable; 
+    }
+
+    if (req.http.X-Timer) {
+        set resp.http.X-Timer = req.http.X-Timer ",VE" time.elapsed.msec;
+    }
+
 }
--- tests/routing.js
+++ tests/routing.js
@@ -4,20 +4,68 @@ var request = require('superagent'),
     fs = require('fs'),
     expect = require('chai').expect;
 
-describe('Next backend', function() {
+describe('next.ft.com backend', function() {
   
     var host = 'http://test.next.ft.com.global.prod.fastly.net';
 
-    it('Should respond with a HTTP 200', function (done) {
-
+    // Wrapper around request/expect
+    var status = function (host, status, done, headers) {
+        var headers = headers || {};
         request
-            .get(host + '/hello')
+            .get(host)
+            .set(headers)
             .end(function (err, res) {
                 if (err) throw err;
-                console.log(res.text);
-                expect(res.status).to.equal(200)
-                done();
+                expect(res.status).to.equal(status);
+                done()
+            });
+    } 
+
+    describe('Authorisation', function () {
+
+        it('Should respond HTTP 302 when access is unauthorized', function (done) {
+            status(host, 403, done);
+        })
+        
+        it('Should respond HTTP 200 when client sends the correct credentials', function (done) {
+            status(host, 200, done, {
+                'x-ft-secret': 'HoratioBottomley'
             });
         })
     
+    });
+
+    describe('Caching', function () {
+        
+        it('Should not cache uncachable requests', function (done) {
+            request
+                .put(host)
+                .set({ 'x-ft-secret': 'HoratioBottomley' })
+                .end(function (err, res) {
+                    if (err) throw err;
+                    //expect(res.status).to.equal(405); // FIXME: generates a 503
+                    expect(res.header['x-ft-cachable']).to.equal('false');
+                    done();
+                });
+        })
+        
+
+    })
+
+    describe('Metrics', function () {
+
+        it('Should time each response', function (done) {
+            request
+                .get(host)
+                .set({ 'x-ft-secret': 'HoratioBottomley' })
+                .end(function (err, res) {
+                    if (err) throw err;
+                    expect(res.header['x-timer']).to.match(/S(\d+).(\d+),VS0,VE(\d)/);
+                    done()
+                });
+        })
+        
+    });
+
+
 });
