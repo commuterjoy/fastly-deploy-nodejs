10b7cf3265a4d72dbb5908ebd396beb9049c9dd3
Matt Chadburn <matt.chadburn@ft.com>
2014-08-07 10:25:33 +0100

    Converted to use friendly API names

--- deploy__vcl.js
+++ deploy__vcl.js
@@ -1,89 +1,54 @@
 
-var fastly = require('fastly')(process.env.fastly_apikey);
+// deploy --stage|prod
+
 var service = encodeURIComponent(process.env.fastly_service);
+var fastly = require('fastly')(process.env.fastly_apikey, service);
 var Q = require('q')
 var fs = require('fs')
 var version;
 
-// the VCL we want to deploy
+// The VCL we want to deploy
 var vcl = fs.readFileSync('./src/vcl/default.vcl', { encoding: 'utf-8' });
 
-// 1. Get the latest version
-
 fastly
-    .request('GET', '/service/' + service + '/version')
-    .then(function (res) {
-        
-        //  2. Derive the last version number and clone it
-
+    .getVersions()                                       //   1. Get the latest version
+    .then( function (res) {                                 //   2. derive the last version number and clone it
         var lastVersion = JSON.parse(res).pop().number;
-        return fastly.request('PUT', '/service/' + service + '/version/' + lastVersion + '/clone');
-    
+        return fastly.cloneVcl(lastVersion); 
     })
-    .then(function (res) {
-        
-        // 3. List VCLs
-        
+    .then(function (res) {                                  //   3. list VCLs
         version = JSON.parse(res).number;
-        return fastly.request('GET', '/service/'+service+'/version/'+version+'/vcl');
+        return fastly.getVcl(version)
     })
-    .then(function (res) {
-     
-        // 4. Delete the VCL (all of them)
+    .then(function (res) {                                  //   4. delete the VCL (all of them)
         return Q.all(
             JSON.parse(res).map(function (vcl) {
-                return fastly.request('DELETE', '/service/'+service+'/version/'+version+'/vcl/'+vcl.name);
+                return fastly.deleteVcl(version, vcl.name);
             })
         )
-
     })
-    .then(function (res) {
-
-        // 5. Upload a VCL for a particular service and version
-       
-        console.log('uploading ...');
-
-        return fastly.request('POST', '/service/'+service+'/version/'+version+'/vcl', {
-            name: 'next',
-            content: vcl
-        })
-
+    .then(function (res) {                                  //   5. upload a VCL for a particular service and version
+        return fastly.updateVcl(version, {
+            name: 'next', content: vcl
+        });
     })
-    .then(function (res) {
-   
-        // 5a. Set the 'next' VCL as the main one
-        
-        console.log('uploaded', res);
-        return fastly.request('PUT', '/service/'+service+'/version/'+version+'/vcl/next/main', {
-            name: 'next',
-            content: vcl
-        })
-
+    .then(function (res) {                                  //    6. Set the 'next' VCL as the main one
+        return fastly.setVclAsMain(version, 'next'); 
     })
-    .then(function (res) {
-
-        // 6. Validate the new VCL
-        
-        return fastly.request('GET', '/service/'+service+'/version/'+version+'/validate')
-
+    .then(function (res) {                                  //    7. Validate the new VCL
+        return fastly.validateVcl(version);
     })
-    .then(function (res) {
-        
-        // 7. Activate the new VCL 
-        
+    .then(function (res) {                                  //    8. Activate the new VCL or report an error 
         if (JSON.parse(res).status === 'ok') {
-            return fastly.request('PUT', '/service/'+service+'/version/'+version+'/activate')
+            return fastly.activateVcl(version); 
         } else {
-            throw new Error('vcl is invalid: ' +  res)
+            throw new Error('VCL is invalid: ' +  res)
         }
-
     })
     .then(function (res) { 
-        console.log('all done', res)
+        console.log('New VCL installed and activated', res);
     })
     .catch(function (err) {
         throw new Error(err); 
     })
     .done();
-
-
